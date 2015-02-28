3d81b5d1bf59eebd022f60e076d54d44ec015d52
manuntag <manuntag@ualberta.ca>
2015-02-27 21:41:02 -0800

    Let's hook it up, fellas

--- Next/MainCollectionViewController.m
+++ Next/MainCollectionViewController.m
@@ -7,31 +7,94 @@
 //
 
 #import "MainCollectionViewController.h"
+#import "LocationManager.h"
+#import "WeatherAPIMannager.h"
+#import "Weather.h"
+#import "SugestionCalculator.h"
+#import "Time.h"
+#import "FourSquareApiManager.h"
+#import "MainCollectionViewCell.h"
+#import "FoursquareObject.h"
 
 @interface MainCollectionViewController ()
+@property (nonatomic, strong) Weather *currentWeather;
+@property (nonatomic, strong) NSMutableArray *fourSquareObjects;
 
 @end
 
-@implementation MainCollectionViewController
+@implementation MainCollectionViewController 
 
-static NSString * const reuseIdentifier = @"Cell";
+//static NSString * const reuseIdentifier = @"Cell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = NO;
+    [[LocationManager sharedInstance] startUpdatingLocation];
     
-    // Register cell classes
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+     self.fourSquareObjects = [NSMutableArray array];
     
-    // Do any additional setup after loading the view.
+    [[WeatherAPIMannager sharedInstance] getWheatherDescriptionForLocation:[LocationManager sharedInstance].currentLocation completion:^(Weather *weather) {
+        
+        self.currentWeather = weather;
+        
+        NSLog(@"newWeather: %@, description: %@", self.currentWeather.mainDescription , self.currentWeather.detailDescription);
+        
+        SugestionCalculator * sugestionCalculator = [[SugestionCalculator alloc]init];
+        NSString * partOfWeek = [Time partOfWeek];
+        NSString * sectionOfDay = [Time sectionOfDay];
+        
+        [sugestionCalculator calculateReccomendationArray:partOfWeek sectionOfDay:sectionOfDay mainWeather:self.currentWeather.mainDescription];
+        NSLog(@"%@", self.currentWeather.mainDescription);
+        
+        NSString * randomReccomendation = [sugestionCalculator randomRecomendedSection];
+        NSLog(@"Foursquare section: %@", randomReccomendation);
+        
+        // first foursquare object
+//        FourSquareApiManager * foursquareApiManager= [FourSquareApiManager sharedInstance];
+//        [foursquareApiManager getFoursquareObjectWithLocation:[LocationManager sharedInstance].currentLocation randomReccomendation:randomReccomendation completion:^(FoursquareObject *fourSquareObject) {
+//            
+//            [self.fourSquareObjects addObject:fourSquareObject];
+//            NSLog(@"%@", self.fourSquareObjects);
+////            dispatch_async(dispatch_get_main_queue(), ^{
+//                [self.collectionView reloadData];
+////            });
+//
+//        }];
+        
+    }];
+    
+    for (int i = 0 ; i<10 ; i++) {
+        [self performSelector:@selector(loadFoursquareObjects) withObject:self afterDelay:5.0];
+    }
+
+//    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(void)loadFoursquareObjects {
+    
+    SugestionCalculator * sugestionCalculator = [[SugestionCalculator alloc]init];
+    NSString * partOfWeek = [Time partOfWeek];
+    NSString * sectionOfDay = [Time sectionOfDay];
+    
+    [sugestionCalculator calculateReccomendationArray:partOfWeek sectionOfDay:sectionOfDay mainWeather:self.currentWeather.mainDescription];
+    NSLog(@"%@", self.currentWeather.mainDescription);
+    
+    NSString * randomReccomendation = [sugestionCalculator randomRecomendedSection];
+    
+    //add foursquare objects to array
+    FourSquareApiManager * foursquareApiManager= [FourSquareApiManager sharedInstance];
+    [foursquareApiManager getFoursquareObjectWithLocation:[LocationManager sharedInstance].currentLocation randomReccomendation:randomReccomendation completion:^(FoursquareObject *fourSquareObject) {
+        
+        [self.fourSquareObjects addObject:fourSquareObject];
+        NSLog(@"%@", self.fourSquareObjects);
+        
+        [self.collectionView reloadData];
+    }];
 }
+
+
+
 
 /*
 #pragma mark - Navigation
@@ -46,20 +109,39 @@ static NSString * const reuseIdentifier = @"Cell";
 #pragma mark <UICollectionViewDataSource>
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-#warning Incomplete method implementation -- Return the number of sections
-    return 0;
+
+    return 1;
 }
 
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-#warning Incomplete method implementation -- Return the number of items in the section
-    return 0;
+
+    return self.fourSquareObjects.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    MainCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
     
-    // Configure the cell
+    FoursquareObject * currentObject = self.fourSquareObjects[indexPath.row];
+    
+    cell.nameLabel.text = currentObject.name;
+    cell.shortDescriptionLabel.text = currentObject.shortDescription;
+    cell.ratingLabel.text = currentObject.rating;
+    
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        NSURL *imageUrl = currentObject.photoUrl;
+        
+        NSData * imageData = [NSData dataWithContentsOfURL:imageUrl];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            cell.imageView.image = [UIImage imageWithData:imageData];
+            //[self.collectionView reloadData];
+        });
+        
+        
+    });
     
     return cell;
 }
